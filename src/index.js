143a784014cf25f40d4b1ad8154f17aaec7f4336
Livio Brunner <livio.brunner@hotmail.com>
2017-02-03 20:20:22 +0100

    add card

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 require("./app/app.style.scss");
 require("exports?VanillaTilt!../node_modules/vanilla-tilt/dist/vanilla-tilt.min.js");
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('introduction-card').addEventListener("mouseover", () => {
+        document.getElementById('home').className += ' active';
+    });
+    document.getElementById('introduction-card').addEventListener("mouseout", () => {
+        document.getElementById('home').className = '';
+    });
+});
